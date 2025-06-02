d89aff561ea6bacf7ea7b1d1ef4d7b3515ea7a25
Norman Babiak <114837984+Uzimonkey123@users.noreply.github.com>
2025-06-02 18:10:32 +0200

    Delete plantuml-client/src directory

--- plantuml-client/src/editor-provider.ts
+++
@@ -1,8 +0,0 @@
-import { GlspEditorProvider, GlspVscodeConnector, SetEditModeAction, RequestModelAction } from '@eclipse-glsp/vscode-integration';
-import * as vscode from 'vscode';
-
-export default class PumlEditorProvider extends GlspEditorProvider {
-    diagramType = "sequence-diagram";
-    extensionContext: vscode.ExtensionContext;
-
-  }
